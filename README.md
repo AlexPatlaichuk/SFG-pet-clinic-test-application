7695745f9389e3025f74939a095436f74a8dfb87
AlexPatlaichuk <alexpatlaichuk@gmail.com>
2023-04-01 13:01:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SFG-pet-clinic-test-application
+Test application done using spring boot framework as a part of an online course
---
+++ src/main/java/guru/springframework/sfgpetclinictestapplication/SfgPetClinicTestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinictestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicTestApplication.class, args);
+    }
+
+}
