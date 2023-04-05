b8855393a21e709271d78345db5860b03d1eee0a
AlexPatlaichuk <alexpatlaichuk@gmail.com>
2023-04-05 13:16:51 +0300

    Implemented base entity. Closes #10

--- SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/model/Person.java
+++ SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/model/Person.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinictestapplication.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
