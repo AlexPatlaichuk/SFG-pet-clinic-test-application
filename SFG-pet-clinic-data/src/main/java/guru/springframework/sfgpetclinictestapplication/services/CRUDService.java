3ac55523bd3ddb6f672f80bce77e38cd93e04128
AlexPatlaichuk <alexpatlaichuk@gmail.com>
2023-04-09 14:00:36 +0300

    Implemented common interface for services. Closes #11

---
+++ SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/services/CRUDService.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinictestapplication.services;
+
+import java.util.Set;
+
+public interface CRUDService<T, ID>{
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
--- SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/services/PetService.java
+++ SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/services/PetService.java
@@ -5,12 +5,5 @@ import guru.springframework.sfgpetclinictestapplication.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save (Pet pet);
-
-    Set<Pet> findAll();
-
+public interface PetService extends CRUDService<Pet, Long> {
 }
