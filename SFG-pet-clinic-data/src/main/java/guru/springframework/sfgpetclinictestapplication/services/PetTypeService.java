f30d58d037cb0983fd9910139db43fb7cb3564a9
Alex Patlaichuk <129276189+AlexPatlaichuk@users.noreply.github.com>
2023-04-21 14:02:48 +0300

    added pet type service. closes #29

---
+++ SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinictestapplication.services;
+
+import guru.springframework.sfgpetclinictestapplication.model.PetType;
+
+public interface PetTypeService extends CRUDService<PetType, Long>{
+}
