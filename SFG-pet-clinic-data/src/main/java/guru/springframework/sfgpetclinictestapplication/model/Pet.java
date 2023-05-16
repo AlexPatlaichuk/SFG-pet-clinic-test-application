ef496a90e029f7cca2e163476d7dbdb905a1278c
Alex Patlaichuk <129276189+AlexPatlaichuk@users.noreply.github.com>
2023-05-16 11:07:48 +0300

    Updated visits with annotations. closes #37

--- SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/model/Pet.java
+++ SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/model/Pet.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgpetclinictestapplication.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +23,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public PetType getPetType() {
         return petType;
@@ -52,5 +57,13 @@ public class Pet extends BaseEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
