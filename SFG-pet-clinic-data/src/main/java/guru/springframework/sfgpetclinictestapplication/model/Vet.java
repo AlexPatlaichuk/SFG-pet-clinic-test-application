1e4ff748b451c9370a3ae05661f218fa23649d6d
Alex Patlaichuk <129276189+AlexPatlaichuk@users.noreply.github.com>
2023-05-17 15:56:56 +0300

    added project lombok, refactored models and dataloader. closes #51

--- SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/model/Vet.java
+++ SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/model/Vet.java
@@ -1,23 +1,27 @@
 package guru.springframework.sfgpetclinictestapplication.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
+
+    @Builder
+    public Vet(Long id, String firstName, String lastName, Set<Specialty> specialties) {
+        super(id, firstName, lastName);
+        this.specialties = specialties;
+    }
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vets_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
