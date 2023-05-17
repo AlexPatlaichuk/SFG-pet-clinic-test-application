963278da7c575c986bd6191461303b6983edb505
Alex Patlaichuk <129276189+AlexPatlaichuk@users.noreply.github.com>
2023-05-17 14:25:52 +0300

    Implemented Map service impl for visits. closes #46

---
+++ SFG-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinictestapplication/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinictestapplication.services;
+
+import guru.springframework.sfgpetclinictestapplication.model.Visit;
+
+public interface VisitService extends CRUDService<Visit, Long> {
+}
